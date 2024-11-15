eb12729022f886885879d138d6a728fac89b4ef6
Matti Henriksen <henrim3@rpi.edu>
2024-11-14 20:54:02 -0500

    add ability to run commands

--- main.hs
+++ main.hs
@@ -4,3 +4,45 @@ import TodoTask
 import TodoState
 import TodoParser
 import TodoUI
+import Control.Monad
+import Control.Monad.IO.Class
+
+main :: IO ()
+main = do
+  putStrLn "Todo App"
+  mainLoop (S (const ((), [])))
+
+
+mainLoop :: TodoListState TodoList () -> IO ()
+mainLoop state = do
+  putStrLn "Enter command:"
+  input <- getLine
+  case parseCommand input of
+    Just command -> do
+      let newState = executeCommand command
+      printState newState
+      mainLoop newState
+    Nothing -> do
+      putStrLn "Invalid command, please try again."
+      mainLoop state
+
+executeCommand :: Command -> TodoListState TodoList ()
+executeCommand (AddTask desc prio dueDate) = do
+  addTask desc prio dueDate
+
+executeCommand (RemoveTask idx) = do
+  removeTaskByIdx idx
+
+executeCommand (CompleteTask idx) = do
+  markTaskCompleteByIdx idx
+
+executeCommand ListTasks = do
+  list <- getTodoList
+  return ()
+
+printState :: TodoListState TodoList () -> IO ()
+printState state = do
+  let ((), list) = runState state []
+  liftIO $ putStrLn "Current Todo List:"
+  liftIO $ mapM_ print list
+
